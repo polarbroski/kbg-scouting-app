ec2e7f6a18902e296c60a196d73413cbfbe85d74
polarbroski <166693319+polarbroski@users.noreply.github.com>
2025-06-18 19:12:44 +0900

    Update rag_llm.py

--- scouting_mvp_full_fixed/rag_llm.py
+++ scouting_mvp_full_fixed/rag_llm.py
@@ -1,10 +1,13 @@
 import random
 
 def generate_summary_from_docs(player_name, documents):
-    # 샘플 리포트 파일명 리스트
-    sample_reports = [
-        "23.png"
+    sample_images = [
+        'lucas_giolito.png',
+        'ronald_acuna.png',
+        'cj_abrams.png',
+        'albert_abreu.png',
+        'chance_adams.png',
+        'yadier_alvarez.png',
+        'osvaldo_abreu.png'
     ]
-    
-    # 무작위 선택
-    return random.choice(sample_reports)
+    return random.choice(sample_images)
