fe08167694024a8fb0bf3ce60a7cdaf9be517232
polarbroski <166693319+polarbroski@users.noreply.github.com>
2025-06-18 19:15:16 +0900

    Update rag_llm.py

--- scouting_mvp_full_fixed/rag_llm.py
+++ scouting_mvp_full_fixed/rag_llm.py
@@ -2,12 +2,7 @@ import random
 
 def generate_summary_from_docs(player_name, documents):
     sample_images = [
-        'lucas_giolito.png',
-        'ronald_acuna.png',
-        'cj_abrams.png',
-        'albert_abreu.png',
-        'chance_adams.png',
-        'yadier_alvarez.png',
-        'osvaldo_abreu.png'
+        '23.png',
+        
     ]
     return random.choice(sample_images)
